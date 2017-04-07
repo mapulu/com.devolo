5c95b8176d0b47fc83c29a0521ddcd9d8e4d16f9
RobinBol <robinbolscher@gmail.com>
2017-04-07 12:19:40 +0200

    MT2647: driver clean up

--- drivers/MT2647/driver.js
+++ drivers/MT2647/driver.js
@@ -3,223 +3,195 @@
 const path = require('path');
 const ZwaveDriver = require('homey-zwavedriver');
 
-module.exports = new ZwaveDriver( path.basename(__dirname), {
+module.exports = new ZwaveDriver(path.basename(__dirname), {
+	debug: true,
 	capabilities: {
-		alarm_contact: {
-			'command_class': 'COMMAND_CLASS_SENSOR_BINARY',
-			'command_report': 'SENSOR_BINARY_REPORT',
-			'command_report_parser': report => {
-				if (report['Sensor Type'] === 'Door/Window')
-					return report['Sensor Value'] === 'detected an event';
-
-				return null;
-			}
-		},
-
 		alarm_motion: [
 			{
-				'command_class': 'COMMAND_CLASS_SENSOR_BINARY',
-				'command_report': 'SENSOR_BINARY_REPORT',
-				'command_report_parser': report => {
-					if (report['Sensor Type'] === 'Motion')
-						return report['Sensor Value'] === 'detected an event';
-
-					return null;
-				}
+				getOnWakeUp: true,
+				command_class: 'COMMAND_CLASS_SENSOR_BINARY',
+				command_report: 'SENSOR_BINARY_REPORT',
+				command_get_parser: () => ({
+					'Sensor Type': 'Motion',
+				}),
+				command_report_parser: report => {
+					if (!report || report['Sensor Type'] !== 'Motion') return null;
+					return report['Sensor Value'] === 'detected an event';
+				},
 			},
 			{
-				'command_class': 'COMMAND_CLASS_BASIC',
-				'command_report': 'BASIC_SET',
-				'command_report_parser': report => {
-					if (report.Value === 0)
-						return false;
-
-					return null;
-				}
-			}
-		],
-
-		alarm_tamper: {
-			'getOnWakeUp': true,
-			'command_class': 'COMMAND_CLASS_SENSOR_BINARY',
-			'command_get': 'SENSOR_BINARY_GET',
-			'command_get_parser': () => ({
-				'Sensor Type': 'Tamper'
-			}),
-			'command_report': 'SENSOR_BINARY_REPORT',
-			'command_report_parser': report => {
-				if (report['Sensor Type'] === 'Tamper')
-					return report['Sensor Value'] === 'detected an event';
-
-				return null;
-			}
-		},
-
-		measure_luminance_level: {
-			'command_class': 'COMMAND_CLASS_SENSOR_MULTILEVEL',
-			'command_report': 'SENSOR_MULTILEVEL_REPORT',
-			'command_report_parser': (report, node) => {
-				if (report['Sensor Type'] === 'Luminance (version 1)') {
-
-						const tokens = { 'luminance': report['Sensor Value (Parsed)'] };
-						Homey.manager('flow').triggerDevice('TSP01_brightness', tokens, null, node.device_data);
-
-						return report['Sensor Value (Parsed)'];
-				}
-
-				return null;
-			}
-		},
-
-		measure_temperature: {
-			'command_class': 'COMMAND_CLASS_SENSOR_MULTILEVEL',
-			'command_report': 'SENSOR_MULTILEVEL_REPORT',
-			'command_report_parser': report => {
-				if (report['Sensor Type'] === "Temperature (version 1)" &&
-				report.hasOwnProperty("Level") &&
-				report.Level.hasOwnProperty("Scale")) {
-					if (report.Level.Scale === 0)
-						return report['Sensor Value (Parsed)'];
-
-					if (report.Level.Scale === 1)
-						return (report['Sensor Value (Parsed)'] - 32) / 1.8;
-
-					return null;
-				}
-
-				return null;
-			}
-		},
-
-    measure_battery: {
-        command_class: 'COMMAND_CLASS_BATTERY',
-        command_get: 'BATTERY_GET',
-        command_report: 'BATTERY_REPORT',
-			  command_report_parser: (report, node) => {
-			    // If prev value is not empty and new value is empty
-			    if (node && node.state && node.state.measure_battery !== 1 && report['Battery Level (Raw)'][0] == 0xFF) {
-
-			      // Trigger device flow
-			      Homey.manager('flow').triggerDevice('battery_alarm', {}, {}, node.device_data, err => {
-			        if (err) console.error('Error triggerDevice -> battery_alarm', err);
-			      });
-			    }
-			    if (report['Battery Level (Raw)'][0] == 0xFF) return 1;
-			      return report['Battery Level (Raw)'][0];
-			        }
-			    }
+				command_class: 'COMMAND_CLASS_BASIC',
+				command_report: 'BASIC_SET',
+				command_report_parser: report => {
+					if (!report || !report.hasOwnProperty('Value')) return null;
+					return report.Value !== 0;
 				},
-	settings: {
-		"basic_set_level": {
-			"index": 2,
-			"size": 1,
-			"signed": false,
-			"parser": input => {
-				if (input >= 100 && input < 255)
-					input = 255;
-
-				return new Buffer([input]);
+			},
+		],
+		alarm_tamper: {
+			getOnWakeUp: true,
+			command_class: 'COMMAND_CLASS_SENSOR_BINARY',
+			command_get: 'SENSOR_BINARY_GET',
+			command_get_parser: () => ({
+				'Sensor Type': 'Tamper',
+			}),
+			command_report: 'SENSOR_BINARY_REPORT',
+			command_report_parser: report => {
+				if (!report || report['Sensor Type'] !== 'Tamper') return null;
+				return report['Sensor Value'] === 'detected an event';
 			},
 		},
-		"pir_sensitivity": {
-			"index": 3,
-			"size": 1,
+		measure_luminance: {
+			command_class: 'COMMAND_CLASS_SENSOR_MULTILEVEL',
+			command_report: 'SENSOR_MULTILEVEL_REPORT',
+			command_report_parser: report => {
+				if (report.hasOwnProperty('Sensor Type') && report.hasOwnProperty('Sensor Value (Parsed)')) {
+					if (report['Sensor Type'] === 'Luminance (version 1)') return report['Sensor Value (Parsed)'];
+				}
+				return null;
+			},
 		},
-		"light_sensitivity": {
-			"index": 4,
-			"size": 1,
+		measure_temperature: {
+			command_class: 'COMMAND_CLASS_SENSOR_MULTILEVEL',
+			command_report: 'SENSOR_MULTILEVEL_REPORT',
+			command_report_parser: report => {
+				if (report.hasOwnProperty('Sensor Type') && report.hasOwnProperty('Sensor Value (Parsed)') &&
+					report.hasOwnProperty('Level') && report.Level.hasOwnProperty('Scale')) {
+					if (report['Sensor Type'] === 'Temperature (version 1)') {
+						if (report.Level.Scale === 1) {
+							return (report['Sensor Value (Parsed)'] - 32) / 1.8;
+						} else if (report.Level.Scale === 0) {
+							return report['Sensor Value (Parsed)'];
+						}
+					}
+				}
+				return null;
+			},
+
 		},
-		"test_mode": {
-			"index": 5,
-			"size": 1,
-			"parser": (input, settings) => {
+		measure_battery: {
+			getOnWakeUp: true,
+			command_class: 'COMMAND_CLASS_BATTERY',
+			command_get: 'BATTERY_GET',
+			command_report: 'BATTERY_REPORT',
+			command_report_parser: (report, node) => {
+				if (report['Battery Level'] === 'battery low warning') {
+					if (node && (!node.state.hasOwnProperty('alarm_battery') || node.state.alarm_battery !== true)) {
+						node.state.alarm_battery = true;
+						module.exports.realtime(node.device_data, 'alarm_battery', true);
+					}
+					return 1;
+				}
+				if (report.hasOwnProperty('Battery Level (Raw)')) {
+					if (node && (!node.state.hasOwnProperty('alarm_battery') || node.state.alarm_battery !== false)) {
+						node.state.alarm_battery = false;
+						module.exports.realtime(node.device_data, 'alarm_battery', false);
+					}
+					return report['Battery Level (Raw)'][0];
+				}
+				return null;
+			},
+		},
+		alarm_battery: {
+			command_class: 'COMMAND_CLASS_BATTERY',
+		},
+	},
+	settings: {
+		basic_set_level: {
+			index: 2,
+			size: 1,
+			signed: false,
+			parser: input => {
+				return new Buffer([(input >= 100 && input < 255) ? 255 : input]);
+			},
+		},
+		pir_sensitivity: {
+			index: 3,
+			size: 1,
+		},
+		turn_off_light_time: {
+			index: 9,
+			size: 1,
+		},
+		light_sensitivity: {
+			index: 4,
+			size: 1,
+		},
+		test_mode: {
+			index: 5,
+			size: 1,
+			parser: (input, settings) => {
 				// Operation mode bit 0 (0000000x)
 				let param5 = Number(settings.operation_mode);
 
 				// Operation mode bit 1 (000000x0)
-				if (input)
-					param5 += 2;
+				if (input) { param5 += 2; }
 
 				// Operation mode bit 2 (00000x00)
-				if (settings["door/window_mode"])
-					param5 += 4;
+				if (settings['door/window_mode']) { param5 += 4; }
 
 				return new Buffer([param5 + 8]);
 			},
 		},
-		"operation_mode": {
-			"index": 5,
-			"size": 1,
-			"parser": (input, settings) => {
+		operation_mode: {
+			index: 5,
+			size: 1,
+			parser: (input, settings) => {
 				// Operation mode bit 0 (0000000x)
 				let param5 = Number(input);
 
 				// Operation mode bit 1 (000000x0)
-				if (settings.test_mode)
-					param5 += 2;
+				if (settings.test_mode) { param5 += 2; }
 
 				// Operation mode bit 2 (00000x00)
-				if (settings["door/window_mode"])
-					param5 += 4;
+				if (settings['door/window_mode']) { param5 += 4; }
 
 				return new Buffer([param5 + 8]);
 			},
 		},
-		"door/window_mode": {
-			"index": 5,
-			"size": 1,
-			"parser": (input, settings) => {
+		'door/window_mode': {
+			index: 5,
+			size: 1,
+			parser: (input, settings) => {
 				// Operation mode bit 0 (0000000x)
 				let param5 = Number(settings.operation_mode);
 
 				// Operation mode bit 1 (000000x0)
-				if (settings.test_mode)
-					param5 += 2;
+				if (settings.test_mode) { param5 += 2; }
 
 				// Operation mode bit 2 (00000x00)
-				if (input)
-					param5 += 4;
+				if (input) { param5 += 4; }
 
 				return new Buffer([param5 + 8]);
 			},
 		},
-		"temperature_monitoring": {
-			"index": 6,
-			"size": 1,
-			"parser": input => {
+		temperature_monitoring: {
+			index: 6,
+			size: 1,
+			parser: input => {
 				// Multi-Sensor Function Switch bit 6 (0x000000)
 				let param6 = 4;	// Default value: Disable magetic integrate PIR
 
-				if (input)
-					param6 += 64;
+				if (input) { param6 += 64; }
 
 				return new Buffer([param6]);
 			},
 		},
-		"pir_redetect_interval_time": {
-			"index": 8,
-			"size": 1,
-			"parser": value => new Buffer([Math.round(value / 8)]),
+		pir_redetect_interval_time: {
+			index: 8,
+			size: 1,
 		},
-		"turn_off_light_time": {
-			"index": 9,
-			"size": 1,
-			"parser": value => new Buffer([Math.round(value / 8)]),
+		battery_report_time: {
+			index: 10,
+			size: 1,
 		},
-		"battery_report_time": {
-			"index": 10,
-			"size": 1,
-			"parser": value => new Buffer([Math.round(value * 2)]),
+		illumination_report_time: {
+			index: 12,
+			size: 1,
 		},
-		"illumination_report_time": {
-			"index": 12,
-			"size": 1,
-			"parser": value => new Buffer([Math.round(value * 2)]),
+		temperature_report_time: {
+			index: 13,
+			size: 1,
 		},
-		"temperature_report_time": {
-			"index": 13,
-			"size": 1,
-			"parser": value => new Buffer([Math.round(value * 2)]),
-		},
-	}
+	},
 });
