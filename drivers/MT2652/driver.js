8d0c9de19f54bde673d1a6d1f0f6df8cd46d314d
RobinBol <robinbolscher@gmail.com>
2017-05-08 12:53:00 +0200

    Version bump (v1.1.1): MT2652: fix battery parser, fix flow card triggers

--- drivers/MT2652/driver.js
+++ drivers/MT2652/driver.js
@@ -6,23 +6,17 @@ const ZwaveDriver = require('homey-zwavedriver');
 // http://products.z-wavealliance.org/products/1143
 
 module.exports = new ZwaveDriver(path.basename(__dirname), {
+	debug: true,
 	capabilities: {
 		measure_battery: {
 			command_class: 'COMMAND_CLASS_BATTERY',
 			command_get: 'BATTERY_GET',
 			command_report: 'BATTERY_REPORT',
-			command_report_parser: (report, node) => {
-				// If prev value is not empty and new value is empty
-				if (node && node.state && node.state.measure_battery !== 1 && report['Battery Level (Raw)'][0] == 0xFF) {
-					// Trigger device flow
-					Homey.manager('flow').triggerDevice('battery_alarm', {}, {}, node.device_data, err => {
-						if (err) console.error('Error triggerDevice -> battery_alarm', err);
-					});
-				}
-				if (report['Battery Level (Raw)'][0] == 0xFF) return 1;
-
-				return report['Battery Level (Raw)'][0];
-			},
+			command_report_parser: report => {
+				if (report['Battery Level'] === 'battery low warning') return 1;
+				if (report.hasOwnProperty('Battery Level (Raw)')) return report['Battery Level (Raw)'][0];
+				return null;
+			}
 		},
 	},
 	settings: {
@@ -69,26 +63,22 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 	},
 });
 
-// bind Flow
 module.exports.on('initNode', (token) => {
 	const node = module.exports.nodes[token];
 	if (node) {
 		node.instance.CommandClass.COMMAND_CLASS_CENTRAL_SCENE.on('report', (command, report) => {
 			if (command.name === 'CENTRAL_SCENE_NOTIFICATION') {
+
 				const triggerMap = {
 					1: '1_single',
 					2: '2_single',
-					3: '3_single',
-					4: '4_single',
-					5: '1_double',
-					6: '2_double',
-					7: '3_double',
-					8: '4_double',
+					5: '3_single',
+					6: '4_single',
 				};
 
-				const triggerId = triggerMap[report['Scene ID']];
+				const triggerId = triggerMap[report['Scene Number']];
 				if (triggerId) {
-					Homey.manager('flow').triggerDevice(`mt265_btn${triggerId}`, null, null, node.device_data);
+					Homey.manager('flow').triggerDevice(`mt2652_btn${triggerId}`, null, null, node.device_data);
 				}
 			}
 		});
