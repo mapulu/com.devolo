56247797791f2dcafe373d6a0ed6591efc8c117e
Matthew Waanders <matthew@athom.nl>
2018-08-02 12:20:05 +0200

    MT2648 rewritten

--- drivers/MT2648/driver.js
+++ drivers/MT2648/driver.js
@@ -1,128 +1,25 @@
 'use strict';
 
-const path = require('path');
-const ZwaveDriver = require('homey-zwavedriver');
+const Homey = require('homey');
 
-module.exports = new ZwaveDriver(path.basename(__dirname), {
-	capabilities: {
-		alarm_contact: {
-			command_class: 'COMMAND_CLASS_SENSOR_BINARY',
-			command_report: 'SENSOR_BINARY_REPORT',
-			command_report_parser: report => {
-				if (report['Sensor Type'] === 'Door/Window') {
-					return report['Sensor Value'] === 'detected an event';
-				}
-				return null;
-			},
-		},
-		alarm_motion: [
-			{
-				getOnWakeUp: true,
-				command_class: 'COMMAND_CLASS_SENSOR_BINARY',
-				command_report: 'SENSOR_BINARY_REPORT',
-				command_get_parser: () => ({
-					'Sensor Type': 'Motion',
-				}),
-				command_report_parser: report => {
-					if (!report || report['Sensor Type'] !== 'Motion') return null;
-					return report['Sensor Value'] === 'detected an event';
-				},
-			},
-			{
-				command_class: 'COMMAND_CLASS_BASIC',
-				command_report: 'BASIC_SET',
-				command_report_parser: report => {
-					if (!report || !report.hasOwnProperty('Value')) return null;
-					return report.Value !== 0;
-				},
-			},
-		],
-		alarm_tamper: {
-			getOnWakeUp: true,
-			command_class: 'COMMAND_CLASS_SENSOR_BINARY',
-			command_get: 'SENSOR_BINARY_GET',
-			command_get_parser: () => ({
-				'Sensor Type': 'Tamper',
-			}),
-			command_report: 'SENSOR_BINARY_REPORT',
-			command_report_parser: report => {
-				if (!report || report['Sensor Type'] !== 'Tamper') return null;
-				return report['Sensor Value'] === 'detected an event';
-			},
-		},
-		measure_luminance: {
-			command_class: 'COMMAND_CLASS_SENSOR_MULTILEVEL',
-			command_report: 'SENSOR_MULTILEVEL_REPORT',
-			command_report_parser: report => {
-				if (report.hasOwnProperty('Sensor Type') && report.hasOwnProperty('Sensor Value (Parsed)')) {
-					if (report['Sensor Type'] === 'Luminance (version 1)') return report['Sensor Value (Parsed)'];
-				}
-				return null;
-			},
-		},
-		measure_temperature: {
-			command_class: 'COMMAND_CLASS_SENSOR_MULTILEVEL',
-			command_report: 'SENSOR_MULTILEVEL_REPORT',
-			command_report_parser: report => {
-				if (report.hasOwnProperty('Sensor Type') && report.hasOwnProperty('Sensor Value (Parsed)') &&
-					report.hasOwnProperty('Level') && report.Level.hasOwnProperty('Scale')) {
-					if (report['Sensor Type'] === 'Temperature (version 1)') {
-						if (report.Level.Scale === 1) {
-							return (report['Sensor Value (Parsed)'] - 32) / 1.8;
-						} else if (report.Level.Scale === 0) {
-							return report['Sensor Value (Parsed)'];
-						}
-					}
-				}
-				return null;
-			},
+// http://products.z-wavealliance.org/products/1143
 
-		},
-		measure_battery: {
-			getOnWakeUp: true,
-			command_class: 'COMMAND_CLASS_BATTERY',
-			command_get: 'BATTERY_GET',
-			command_report: 'BATTERY_REPORT',
-			command_report_parser: report => {
-				if (report['Battery Level'] === 'battery low warning') {
-					return 1;
-				}
-				if (report.hasOwnProperty('Battery Level (Raw)')) {
-					return report['Battery Level (Raw)'][0];
-				}
-				return null;
-			},
-		},
-	},
-	settings: {
-		basic_set_level: {
-			index: 2,
-			size: 1,
-			signed: false,
-			parser: input => {
-				if (input >= 100 && input < 255) { input = 255; }
-				return new Buffer([input]);
-			},
-		},
-		light_sensitivity: {
-			index: 4,
-			size: 1,
-		},
-		battery_report_time: {
-			index: 10,
-			size: 1,
-		},
-		contact_report_time: {
-			index: 11,
-			size: 1,
-		},
-		illumination_report_time: {
-			index: 12,
-			size: 1,
-		},
-		temperature_report_time: {
-			index: 13,
-			size: 1,
-		},
-	},
-});
+class DevoloContactSensor extends ZwaveDevice {
+
+	onMeshInit() {
+		this.registerCapability('alarm_contact', 'SENSOR_BINARY');
+        this.registerCapability('alarm_motion', 'BASIC');
+        this.registerCapability('alarm_tamper', 'SENSOR_BINARY');
+        this.registerCapability('measure_luminance', 'SENSOR_MULTILEVEL');
+        this.registerCapability('measure_temperature', 'SENSOR_MULTILEVEL');
+        this.registerCapability('measure_battery', 'BATTERY');
+
+        this.registerSetting('basic_set_level', input => {
+            if (input >= 100 && input < 255) { input = 255; }
+            return new Buffer([input]);
+        });
+    }
+
+}
+
+module.exports = DevoloContactSensor;
