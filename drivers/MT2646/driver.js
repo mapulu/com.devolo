daceb01da9e25aff6e2936abb70ac04ffa176c87
Matthew Waanders <matthew@athom.nl>
2018-08-20 16:05:13 +0200

    Implementation for MT2646

--- drivers/MT2646/driver.js
+++ drivers/MT2646/driver.js
@@ -1,80 +1,16 @@
 'use strict';
 
-const path = require('path');
-const ZwaveDriver = require('homey-zwavedriver');
+const Homey = require('homey');
+const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
 
-module.exports = new ZwaveDriver(path.basename(__dirname), {
-	capabilities: {
-		onoff: {
-			command_class: 'COMMAND_CLASS_SWITCH_BINARY',
-			command_get: 'SWITCH_BINARY_GET',
-			command_set: 'SWITCH_BINARY_SET',
-			command_set_parser: value => ({
-				'Switch Value': (value > 0) ? 'on/enable' : 'off/disable',
-			}),
-			command_report: 'SWITCH_BINARY_REPORT',
-			command_report_parser: report => report.Value === 'on/enable',
-		},
-		measure_power: {
-			command_class: 'COMMAND_CLASS_METER',
-			command_get: 'METER_GET',
-			command_get_parser: () => ({
-				'Sensor Type': 'Electric meter',
-				Properties1: {
-					Scale: 0,
-				},
-			}),
-			command_report: 'METER_REPORT',
-			command_report_parser: report => {
-				if (report.hasOwnProperty('Properties2') &&
-					report.Properties2.hasOwnProperty('Scale bits 10') &&
-					report.Properties2['Scale bits 10'] === 2) {
-					return report['Meter Value (Parsed)'];
-				}
-				return null;
-			},
-		},
-		meter_power: {
-			command_class: 'COMMAND_CLASS_METER',
-			command_get: 'METER_GET',
-			command_get_parser: () => ({
-				'Sensor Type': 'Electric meter',
-				Properties1: {
-					Scale: 2,
-				},
-			}),
-			command_report: 'METER_REPORT',
-			command_report_parser: report => {
-				if (report.hasOwnProperty('Properties2') &&
-					report.Properties2.hasOwnProperty('Scale bits 10') &&
-					report.Properties2['Scale bits 10'] === 0) {
-					return report['Meter Value (Parsed)'];
-				}
-				return null;
-			},
-		},
-	},
-	settings: {
-		1: {
-			size: 2,
-			index: 1,
-		},
-		2: {
-			size: 2,
-			index: 2,
-		},
-		5: {
-			size: 1,
-			index: 5,
-		},
+class DevoloMeteringPlug extends ZwaveDevice {
 
-		7: {
-			size: 1,
-			index: 7,
-		},
-		8: {
-			size: 2,
-			index: 8,
-		},
-	},
-});
+	onMeshInit() {
+		this.registerCapability('onoff', 'SWITCH_BINARY');
+		this.registerCapability('measure_power', 'METER');
+		this.registerCapability('meter_power', 'METER');
+	}
+
+}
+
+module.exports = DevoloMeteringPlug;
